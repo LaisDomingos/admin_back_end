5abc14aa0493ebcb1eb044144b5d0e49f380ef30
Laís Santos <laisdomingos0@gmail.com>
2024-12-13 16:01:29 -0300

    cors with localhost

--- api/createDriver.js
+++ api/createDriver.js
@@ -1,8 +1,17 @@
 import connectToDatabase from '../lib/dbConnect'; // Importa a conexão com o banco de dados
 
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://admin-front-end-pied.vercel.app',
+];
+
 export default async (req, res) => {
-  // Configuração de CORS
-  res.setHeader('Access-Control-Allow-Origin', 'https://admin-front-end-pied.vercel.app'); // Permite apenas o frontend especificado
+  // Configuração dinâmica de CORS
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin); // Define o origin dinamicamente
+  }// Permite apenas o frontend especificado e o localhost
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Métodos permitidos
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Headers permitidos
 
