ce4e1bf0f8dc1036b34f99effa5f53f8176229b5
Laís Santos <laisdomingos0@gmail.com>
2024-12-18 16:31:01 -0300

    Change Post API Driver again 4

--- api/post/createDriver.js
+++ api/post/createDriver.js
@@ -11,7 +11,10 @@ export default async (req, res) => {
 
   if (allowedOrigins.includes(origin)) {
     res.setHeader('Access-Control-Allow-Origin', origin); // Define o origin dinamicamente
-  }// Permite apenas o frontend especificado e o localhost
+  } else {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Para outras origens, considere usar '*', se necessário
+  }
+
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Métodos permitidos
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Headers permitidos
 
@@ -21,8 +24,10 @@ export default async (req, res) => {
   }
 
   if (req.method === 'POST') {
-    const { name, rut, supplyer_id } = req.body;
-    if (!name || !rut || !supplyer_id) {
+    const { name, rutNumber, supplyer_id } = req.body;
+    
+    // Verificando se os dados necessários estão presentes
+    if (!name || !rutNumber || !supplyer_id) {
       return res.status(400).json({ message: 'Nome, RUT e Supplyer_id são obrigatórios!' });
     }
 
@@ -30,18 +35,21 @@ export default async (req, res) => {
       const { db } = await connectToDatabase();
       const collection = db.collection('drivers');
 
-      const result = await collection.insertOne({ name, rut, supplyer_id});
+      // Inserindo o motorista no banco de dados
+      const result = await collection.insertOne({ name, rutNumber, supplyer_id });
 
+      // Retorno com dados do novo motorista
       return res.status(201).json({
         message: 'Motorista criado com sucesso!',
-        driver: { name, rut, supplyer_id},
+        driver: { name, rutNumber, supplyer_id },
         _id: result.insertedId,
       });
     } catch (error) {
-      console.error(error);
+      console.error('Erro ao salvar motorista:', error); // Mensagem de erro mais informativa
       return res.status(500).json({ message: 'Erro ao salvar motorista' });
     }
   }
 
+  // Método não permitido para outros tipos de requisição
   res.status(405).json({ message: 'Método não permitido' });
 };
